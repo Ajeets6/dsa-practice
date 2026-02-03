7f853aa56d1fbd0ac136d12e1370e43ab2a09148
Ajeets6 <itachis6234@gmail.com>
2026-02-03 22:36:04 +1100

    Number of subarray with at most K sum

---
+++ questions/SubarrayAtMostKsum.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS                \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0);
+
+int main() {
+  IOS
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int a[n];
+        for(int i = 0; i < n; i++) cin >> a[i];
+        int left=0,right=0;
+        int sum=0;
+        int count=0;
+        while(right<n){
+            sum+=a[right];
+           while(sum>k){
+
+            sum-=a[left];
+             left++;
+
+        }
+        if(sum<=k){
+        count+=(right-left+1);}
+        right++;
+
+
+    }
+
+    cout<<count<<endl;
+
+    }
+  return 0;
+}
