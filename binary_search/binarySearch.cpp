2120c063891da7c910e42060a061b2a34db0a530
Ajeets6 <itachis6234@gmail.com>
2026-02-15 23:45:52 +1100

    Solved 2 binary search question

---
+++ binary_search/binarySearch.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS \
+  ios::sync_with_stdio(0); \
+  cin.tie(0); \
+  cout.tie(0);
+
+int search(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) left=mid+1;
+            else right=mid-1;
+        }
+        return -1;
+    }
+
+int main() {
+  IOS
+
+
+  return 0;
+}
